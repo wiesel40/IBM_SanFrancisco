f5ff32a14b1fe28fb720437beceb6d923a809822
wiesel40 <35740619+wiesel40@users.noreply.github.com>
2018-01-23 23:13:45 +0100

    Update CSVParser.java

--- IBM_SanFrancisco/src/CSVParser/CSVParser.java
+++ IBM_SanFrancisco/src/CSVParser/CSVParser.java
@@ -6,20 +6,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+This class is responsible for parsing the CSV File 
+*/
 public class CSVParser
 {
   protected String m_filename;
@@ -29,13 +18,16 @@ public class CSVParser
   private CSVFilterParameter m_csvFilter;
   private ArrayList<CSVFilterParameter> m_filterList;
   
+  /*
+    Getter of CSVFilterParameter List which holds the movie name and the movie location
+  */
   public ArrayList<CSVFilterParameter> getM_filterList()
   {
     return m_filterList;
   }
-  
-
-
+  /*
+    Getter and Setter for the filename 
+  */
   public void setM_filename(String m_filename)
   {
     this.m_filename = m_filename;
@@ -45,7 +37,9 @@ public class CSVParser
     return m_filename;
   }
   
-
+  /*
+  This class is responsible for parsing the CSV File 
+  */
   private CSVParser()
     throws FileNotFoundException, IOException
   {
@@ -63,20 +57,22 @@ public class CSVParser
     m_csvFilter = new CSVFilterParameter();
   }
   
-
-
+  /*
+  Reads the CSVFile Current file 
+  */
   public void readFile()
     throws FileNotFoundException, IOException
   {
     m_filereader = new FileReader(m_filename);
     m_reader = new BufferedReader(m_filereader);
-    while (m_reader.readLine() != null)
+    
+    while ((l_line =  m_reader.readLine()) != null)
     {
       String l_moviename = "";
       String l_movieLocation = "";
-      
-      String[] l_array = splitLines(m_reader.readLine());
-      
+     //split current line 
+      String[] l_array = splitLines(l_line);
+      //find out movie name and movie title 
       if (!l_array[0].equals("Title")) {
         l_moviename = l_array[0];
       }
@@ -88,9 +84,9 @@ public class CSVParser
       m_filterList.add(m_csvFilter);
     }
   }
-  
-
-
+  /*
+  Splits the current line in ;
+  */
   private String[] splitLines(String p_line)
   {
     m_filearray = p_line.split(";");
