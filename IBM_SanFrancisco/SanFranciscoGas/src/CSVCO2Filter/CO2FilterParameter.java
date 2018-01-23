6cbd96729b18b59af68e7a3ba9374568494ec202
wiesel40 <35740619+wiesel40@users.noreply.github.com>
2018-01-23 23:06:44 +0100

    Update CO2FilterParameter.java

--- IBM_SanFrancisco/SanFranciscoGas/src/CSVCO2Filter/CO2FilterParameter.java
+++ IBM_SanFrancisco/SanFranciscoGas/src/CSVCO2Filter/CO2FilterParameter.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package CSVCO2Filter;
 
 /**
- *
- * @author Alexander
+ This class provides getter and setter methods for 
+ Department , Emissions_mtCO2e , m_SourceType
  */
 public class CO2FilterParameter {
 
