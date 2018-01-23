3223fb2639b123c18f2b5350eff7c226ad4bab9c
wiesel40 <35740619+wiesel40@users.noreply.github.com>
2018-01-23 23:05:28 +0100

    Update CSVCO2Parser.java

--- IBM_SanFrancisco/SanFranciscoGas/src/CSVCO2Filter/CSVCO2Parser.java
+++ IBM_SanFrancisco/SanFranciscoGas/src/CSVCO2Filter/CSVCO2Parser.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package CSVCO2Filter;
 
 import java.io.BufferedReader;
@@ -13,21 +9,28 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Alexander
+ * Provides a CSV Parser for parsing the data 
  */
 public class CSVCO2Parser {
 
     private String[] m_filearray;
     private BufferedReader m_reader;
     private FileReader m_filereader;
+    // CO2FilterParameter contians Department , Source_Type; Emissions_mtCO2 data
     private CO2FilterParameter m_filterparameter;
     private ArrayList<CO2FilterParameter> m_filter;
     private String m_filename;
 
+    /*
+     Returns a ArrayList with the CO2FilterParameter class
+    */
     public ArrayList<CO2FilterParameter> getCO2FilterParameter() {
         return m_filter;
     }
 
+    /*
+     Getter and Setter Current File Name 
+    */
     public String getFilename() {
         return m_filename;
     }
@@ -47,7 +50,9 @@ public class CSVCO2Parser {
         m_filter = new ArrayList();
         m_filterparameter = new CO2FilterParameter();
     }
-
+/*
+   reads the CSV File 
+*/
     public void readFile() throws FileNotFoundException, IOException, NumberFormatException {
         m_filereader = new FileReader(m_filename);
         m_reader = new BufferedReader(m_filereader);
@@ -59,7 +64,7 @@ public class CSVCO2Parser {
             String l_sourceType = "";
             //split current line 
             String[] l_array = splitLines(l_line);
-            //find out movie name and movie title 
+            //find out Department , Source_Type; Emissions_mtCO2
             if (!l_array[0].equals("Department")) {
                 l_Department = l_array[0];
             }
@@ -68,7 +73,7 @@ public class CSVCO2Parser {
             }
             if (!l_array[4].equals("Emissions_mtCO2e")) {
                 l_Emissions_mtCO2e = l_array[4];
-
+                //Filter check for the right data 
                 if (filterValue(l_Emissions_mtCO2e) != 0.0) {
                     m_filterparameter = new CO2FilterParameter(l_Department, l_sourceType, l_Emissions_mtCO2e);
                     m_filter.add(m_filterparameter);
@@ -77,11 +82,17 @@ public class CSVCO2Parser {
         }
     }
 
+    /*
+    splits the String in ;
+    */
     private String[] splitLines(String p_line) {
         m_filearray = p_line.split(";");
         return m_filearray;
     }
 
+    /*
+        Checks for 0.00
+    */
     private Double filterValue(String p_line){
         Double l_Emissions_mtCO2e = 1.0;
         if (p_line.equals("0.00")) {
